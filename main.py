8a675d572db493e01e24e5772541503f7daf17b6
Alex Damascena <apfd@cin.ufpe.br>
2023-03-27 23:59:18 -0300

    feature(light): add more objects

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ if __name__ == "__main__":
     width = 320
     height = 200
     camera = Vector3D(0, 0, -1)
-    objects = [Sphere(Point(0,0,0), 0.5, Material(Color.from_hex("#FF0000")))]
-    lights = [Light(Point(1.5, -0.5, -10.0), Color.from_hex("#FFFFFF"))]
+    objects = [Sphere(Point(0.75, -0.1, 1),0.6, Material(Color.from_hex("#FF0000"), 0.2)),
+               Sphere(Point(0, 10000.5, 1), 10000, Material(Color.from_hex("#c3c3c3"))),
+               Sphere(Point(-0.75, -0.1, 2.25), 0.6, Material(Color.from_hex("#0000FF")))
+    ]
+    lights = [
+        Light(Point(1.5, -0.5, -10.0), Color.from_hex("#FFFFFF"))]
     scene = Scene(camera, objects, lights, width, height)
     engine = RenderEngine()
     image = engine.render(scene)
