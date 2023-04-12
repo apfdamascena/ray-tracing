5993cddef1a3a8faee0970576ffe96ee1d0392c9
Alex Damascena <apfd@cin.ufpe.br>
2023-04-12 02:53:21 -0300

    feature(ray-tracing): add triangle

---
+++ Triangle.py
@@ -0,0 +1,54 @@
+from Object import Object
+from Material import Material
+from Point import Point
+from Vector3D import Vector3D
+from Ray import Ray
+from Plane import Plane
+
+
+class Triangle(Object):
+
+    def __init__(self, material: Material, a=Point(0.1, 0, 0), b=Point(0, 0.1, 0), c=Point(0, 0, 0.1 )):
+        super().__init__(material)
+
+        self.a = a
+        self.b = b
+        self.c = c
+  
+        c_to_a = self.c - self.a
+        b_to_a = self.b - self.a
+        self._normal = c_to_a.cross_product(b_to_a).normalize()
+
+        self.distance = self._normal.dot_product(self.a)
+
+    def normal(self, surface_point):
+        return self._normal
+
+    def intersect(self, ray):
+        dot = ray.direction.dot_product(self._normal)
+
+        if dot == 0:
+            return None
+
+        dummy = (self._normal.dot_product(ray.origin + (self._normal * self.distance) ) * -1)
+        dist_to_triangle = -1 * dummy / dot
+
+        q = ray.direction * dist_to_triangle + ray.origin
+
+        c_to_a = self.c - self.a
+        qa = q - self.a
+
+        bc = self.b - self.c
+        qc = q - self.c
+
+        ab = self.a - self.b
+        qb = q - self.b
+
+        inside = c_to_a.cross_product(qa).dot_product(self._normal) >= 0 and \
+                    bc.cross_product(qc).dot_product(self._normal) >= 0 and \
+                    ab.cross_product(qb).dot_product(self._normal) >= 0
+
+        if inside:
+            return dist_to_triangle
+        else:
+            return None
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from RenderEngine import RenderEngine
 from Light import Light
 from Material import Material
 from Plane import Plane
+from Triangle import Triangle
 
 if __name__ == "__main__":
     width = 320
@@ -16,7 +17,8 @@ if __name__ == "__main__":
     camera = Camera(Point(0, 0, 0), Point(0,0,1), 50, height, width)
     objects = [Sphere(Point(0.75, -0.1, 1), 0.6, Material(Color.from_hex("#FF0000"), 0.2)),
                Sphere(Point(-0.75, -0.1, 2.25), 0.6, Material(Color.from_hex("#0000FF"))),
-               Plane(Material(Color.from_hex("#FF0000"), 0.2), Point(0, 0.8, 0.2), Point(0, 0.8,0))
+               Plane(Material(Color.from_hex("#FF0000"), 0.2), Point(0, 0.8, 0.2), Point(0, 0.8,0)),
+               Triangle(Material(Color.from_hex("#FFFFFF"), 0.2))
                ]
 
     lights = [ Light(Point(1.5, -0.5, -10.0), Color.from_hex("#FFFFFF")) ]
