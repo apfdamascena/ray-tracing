83b24a22fa6625817e8555b49672d8775767a25f
Alex Damascena <apfd@cin.ufpe.br>
2023-04-23 22:55:47 -0300

    feature(reflection-refraction): refacoting code

--- RenderEngine.py
+++ RenderEngine.py
@@ -11,25 +11,28 @@ class RenderEngine:
         height = scene.height
         camera = scene.camera
 
+        pixel_length = 0.4
+
         z_vector = camera.origin - camera.screen_distance * camera.w
         y_vector = (height / 2) * camera.v
         x_vector = (width / 2 ) * camera.u
-        image_center = z_vector + 0.4 *(y_vector - x_vector)
+        image_center = z_vector + pixel_length * (y_vector - x_vector)
 
         pixels = Image(width, height)
 
         for j in range(height):
             for i in range(width):
-                position = image_center + 0.4 * (camera.u * i - j * camera.v)
+                position = image_center + pixel_length * (camera.u * i - j * camera.v)
                 direction = (position - camera.origin).normalize()
                 ray = Ray(camera.origin, direction)
                 pixels.set_pixel(i, j, self.ray_trace(ray, scene))
         return pixels
 
     def ray_trace(self, ray, scene, depth=0):
-        color = Color(0,0,0)
+
         # find the nearest object hitted by the ray
         distance_hit, object_hit = self.find_nearest(ray, scene)
+        color = Color(0,0,0)
 
         if object_hit is None:
             return scene.ambient_color
@@ -39,60 +42,63 @@ class RenderEngine:
 
         color += self.color_at(object_hit, hit_pos, hit_normal, scene)
 
-        if depth < scene.max_depth:
-            material_hit = object_hit.material
-            if material_hit.reflection > 0:
+        if depth < scene.end_depth:
+            material_hitted = object_hit.material
+            if material_hitted.reflection > 0:
                 omega = -ray.direction
                 normal = normal_hit
-                if omega ^ normal < 0:
-                    normal = -normal
-                new_pos = hit_position + normal * self.MINIMAL_DISPLACE
-                new_dir = ray.direction - 2 * ray.direction.dotProduct(normal) * normal
-                new_ray = Ray(new_pos, new_dir)
-                color += self.rayTrace(new_ray, scene, depth + 1) * material_hit.reflection
 
-            if material_hit.refraction > 0:
+                normal = -normal if omega ^ normal < 0 else normal
+
+                new_ray_position = hit_pos + normal * 0.001
+                new_ray_direction = ray.direction - 2 * ray.direction.dot_product(normal) * normal
+                new_ray = Ray(new_ray_position, new_ray_direction)
+                color += self.ray_trace(new_ray, scene, depth + 1) * material_hitted.reflection
+
+            if material_hitted.refraction > 0:
                 omega = -ray.direction
                 normal = normal_hit
-                relative_refraction = material_hit.refraction
+                relative_refraction = material_hitted.refraction
 
-                if omega ^ normal < 0:
-                    relative_refraction = 1/material_hit.refraction
-                    normal = -normal
+                relative_refraction = 1/material_hitted.refraction if omega ^ normal < 0 else relative_refraction
+                normal = -normal if omega ^ normal < 0 else normal
                 
-            delta = 1-(1/(relative_refraction**2) * (1-(normal ^omega)**2))
+            variation = 1-(1/(relative_refraction**2) * (1-(normal ^ omega)**2))
 
-            if delta >= 0:
-                inverse_refra = 1 / relative_refraction
-                new_dir = -inverse_refra * omega - (math.sqrt(delta) - inverse_refra * (normal ^omega)) * normal
-                new_pos = hit_position - normal * self.MINIMAL_DISPLACE
-                new_ray = Ray(new_pos, new_dir)
+            if variation >= 0:
+                inverse_refraction = 1 / relative_refraction
+                new_ray_direction = -inverse_refraction * omega - (sqrt(variation) - inverse_refraction * (normal ^omega)) * normal
+                new_ray_position = hit_pos - normal * 0.001
+                new_ray = Ray(new_ray_position, new_ray_direction)
 
-                color += self.rayTrace(new_ray, scene, depth +1) * material_hit.transmission
+                color += self.rayTrace(new_ray, scene, depth +1) * material_hitted.transmission
             else:
-                new_pos = hit_position + normal *self.MINIMAL_DISPLACE
-                new_dir = ray.direction - 2 * ray.direction.dotProduct(normal) * normal
-                new_ray = Ray(new_pos, new_dir)
+                new_ray_position = hit_pos + normal * 0.001
+                new_ray_direction = ray.direction - 2 * ray.direction.dot_product(normal) * normal
+                new_ray = Ray(new_ray_position, new_ray_direction)
 
-                color += self.rayTrace(new_ray, scene, depth +1) * material_hit.transmission
+                color += self.ray_trace(new_ray, scene, depth +1) * material_hitted.transmission
         return color
 
     def find_nearest(self, ray, scene):
         distance_min, object_hit = None, None
         for obj in scene.objects:
             dist = obj.intersect(ray)
-            if dist is not None and (object_hit is None or dist < distance_min):
+            if self.isAnyObjectHittedAndHasBetterDistance(dist, object_hit, distance_min):
                 distance_min = dist
                 object_hit = obj
         return (distance_min, object_hit)
 
+    def isAnyObjectHittedAndHasBetterDistance(self, dist, object_hit, distance_min):
+        return dist is not None and (object_hit is None or dist < distance_min)
+
     def color_at(self, object_hit, hit_pos, normal, scene):
         material = object_hit.material
         to_cam = (scene.camera.origin - hit_pos).normalize()
+
         object_color = material.color_at(hit_pos)
         color = material.ambient * (object_color.kron_product(scene.ambient_color))
-        phong = 50
-   
+
         #light calculations
         for light in scene.lights:
             to_light = Ray(hit_pos, light.position - hit_pos)
@@ -100,7 +106,7 @@ class RenderEngine:
             distance_hit, object_hit = self.find_nearest(to_light, scene)
             distance = to_light.direction.dot_product((light.position - hit_pos))
 
-            if object_hit != None and 0 < distance_hit < distance:
+            if self.isDiffuseAndPhongNotNeeded(object_hit, distance_hit, distance):
                 continue
             ## diffuse shadding (Lambert)
 
@@ -111,6 +117,9 @@ class RenderEngine:
             color += light.color * material.specular * max(half_vector.dot_product(to_cam), 0) ** 5
 
         return color
+
+    def isDiffuseAndPhongNotNeeded(self, object_hit, distance_hit, distance):
+        return object_hit != None and 0 < distance_hit < distance
 
 
 
--- Scene.py
+++ Scene.py
@@ -4,11 +4,11 @@ from Color import Color
 
 class Scene:
 
-    def __init__(self, camera, objects, lights, width, height, ambient_color = Color.from_hex("#000000"), max_depth = 0):
+    def __init__(self, camera, objects, lights, width, height, ambient_color = Color.from_hex("#000000"), end_depth = 0):
         self.camera = camera
         self.objects = objects
         self.width = width
         self.height = height
         self.lights = lights
         self.ambient_color = ambient_color
-        self.max_depth = max_depth
+        self.end_depth = end_depth
