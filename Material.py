c953959dc8985df3c59f2b44a6ba44d50e5f266f
claraabk <claraabk1@gmail.com>
2023-04-27 14:46:44 -0300

    fix(ref):scene params and engine variables

--- Material.py
+++ Material.py
@@ -2,7 +2,7 @@ from Color import Color
 
 class Material:
 
-    def __init__(self, color = Color.from_hex("#FFFFFF"), ambient = 0.05, diffuse=1.0, specular = 1.0, reflection = 1.0, transmission = 1.0, roughness = 1.0, refraction=1.20):
+    def __init__(self, color = Color.from_hex("#FFFFFF"), ambient = 0.05, diffuse=1.0, specular = 1.0, reflection = 1.0, transmission = 1.0, roughness = 1.0, refraction=0.8):
         self.color = color
         self.ambient = ambient
         self.diffuse = diffuse
